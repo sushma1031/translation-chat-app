5bfc5c003aa640c52604fa544afa25f23631d967
sushma1031 <sushmajram@gmail.com>
2024-10-14 17:32:47 +0530

    be: translate local image text

--- api/index.py
+++ api/index.py
@@ -8,3 +8,20 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/translate/image", methods=['POST'])
+async def translate_image(): 
+  path = f"api/assets/kannada-1.jpg"
+  
+  # translations = image.translate_img_text(path, 'kannada', 'english')
+  translations = ["Foo bar", "rab ooF"]
+  
+  return jsonify({'translated': translations})
+
+@app.route('/api/translate/text', methods=['POST'])
+def translate_text():
+    data = request.get_json()  # Get JSON data from the request
+    text = data.get('text')    # Extract the text sent from React
+
+    # Return the translated text as a JSON response
+    return jsonify({'translated': text})
