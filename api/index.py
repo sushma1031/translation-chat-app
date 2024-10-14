c8b46783ccf228a8024d8a8d6e52718b437af7b6
sushma1031 <sushmajram@gmail.com>
2024-10-14 17:27:12 +0530

    server: set up api

--- api/index.py
+++ api/index.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from utils import image
+
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 @app.route("/api/python")
 def hello_world():
