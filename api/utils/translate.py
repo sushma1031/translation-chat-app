57a77f87b7ec87305f380d976a5bfd99b41a7db3
sushma1031 <sushmajram@gmail.com>
2024-10-14 18:19:07 +0530

    be: refactor

--- api/utils/translate.py
+++ api/utils/translate.py
@@ -2,8 +2,8 @@ from googletrans import Translator
 
 translator = Translator()
 
-def translate_single(spoken_text, from_language, to_language):
-    return translator.translate(spoken_text, src=from_language, dest=to_language)
+def translate_single(text, from_language, to_language):
+    return translator.translate(text, src=from_language, dest=to_language)
 
 def translate_batch(batch, to_language):
     return translator.translate(batch, dest=to_language)
