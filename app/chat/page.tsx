6300988fde7defa6e2d11fc68a15b883e158781d
sushma1031 <sushmajram@gmail.com>
2024-10-15 13:00:18 +0530

    fe: process audio translation

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,21 +1,52 @@
 "use client";
 import { FormEvent, useState } from "react";
-import axios from "axios";
+import { createAPIConfig } from "../config/config.js";
+import * as Echogarden from "echogarden";
+
+const apiConfig = createAPIConfig();
 
 export default function Chat() {
   const [inputText, setInputText] = useState("");
   const [translatedText, setTranslatedText] = useState("");
-  
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+
+  async function handleAudioSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    const formData = new FormData();
+    let src = "hindi";
+    let files = (e.target as HTMLInputElement).files;
+    if (!files || files.length == 0) {
+      alert("Please upload an audio file");
+      return;
+    }
+
+    let file = files[0];
+
+    formData.append("src", src);
+    formData.append("dest", "english");
+    let response = {
+      data: {
+        translated: "",
+      },
+    };
     try {
-      const response = await axios({
-        method: "post",
-        url: "http://localhost:5328/api/translate/image",
-        data: {
-          text: inputText,
-        },
-      });
+      if (src == "english") {
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+          const arrayBuffer = (e.target && e.target.result) as ArrayBuffer;
+          const uint8Array = new Uint8Array(arrayBuffer);
+          const transcribed = await Echogarden.recognize(uint8Array, {
+            language: "en",
+            whisper: { model: "tiny.en" },
+          });
+          formData.append("transcribed", transcribed.transcript); // send the transcribed text itself
+          // should we have a different endpoint?
+          response = await apiConfig.post("translate/audio", formData);
+        };
+        reader.readAsArrayBuffer(file);
+      } else {
+        formData.append("file", "file");
+        response = await apiConfig.post("translate/audio", formData);
+      }
 
       setTranslatedText(response.data.translated);
     } catch (error) {
@@ -23,10 +54,16 @@ export default function Chat() {
     }
   }
 
+  async function handleVideoSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData();
+    
+  }
+
   return (
     <>
       <h1>Chat Room</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleAudioSubmit}>
         <label htmlFor="userText">Enter text: </label>
         <input
           name="userText"
@@ -34,6 +71,7 @@ export default function Chat() {
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
         />
+        <input type="file" name="audioFile" id="audioFile" />
         <br />
         <button type="submit">Translate</button>
       </form>
