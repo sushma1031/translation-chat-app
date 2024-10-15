078c8669c3ba6875b73652c19ee1d346bf73163d
sushma1031 <sushmajram@gmail.com>
2024-10-15 12:57:55 +0530

    be: translate text

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from utils import image
+from utils import image, text, audio
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
@@ -8,6 +8,16 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/api/translate/text', methods=['POST'])
+async def translate_text():
+    data = request.get_json() # Get JSON data from the request
+    t = data.get('text') # Extract the text sent from React
+
+    # Return the translated text as a JSON response
+    # trans = text.translate_text(t, 'korean', 'english')
+    trans = 'dummy'
+    return jsonify({'translated': trans})
 
 @app.route("/api/translate/image", methods=['POST'])
 async def translate_image(): 
