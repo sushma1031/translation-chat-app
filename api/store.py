5d1ca5316d38bda85d6a165ea4524f989daf90cd
sushma1031 <sushmajram@gmail.com>
2024-10-15 17:02:19 +0530

    be: setup cloud storage

---
+++ api/store.py
@@ -0,0 +1,24 @@
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+import dotenv
+import os
+from pathlib import Path
+
+CLOUD_DIR="trans-chat"
+
+def upload_to_cld(path, type):
+  dotenv.load_dotenv()
+
+  # Configuration       
+  cloudinary.config( 
+    cloud_name = os.environ['CLOUD_NAME'], 
+    api_key = os.environ['API_KEY'], 
+    api_secret = os.environ['API_SECRET'],
+    secure=True
+  )
+  resource = type if type == "image" else "video"
+  filename = f"{CLOUD_DIR}/{type}/{Path(path).stem}"
+  upload_result = cloudinary.uploader.upload(path, public_id=filename, resource_type=resource)
+  return upload_result["secure_url"]
