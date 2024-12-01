76056042a304c6a0628acb853b56f15d869f73fb
sushma1031 <sushmajram@gmail.com>
2024-12-01 17:29:15 +0530

    be: return just translated text

--- api/utils/text.py
+++ api/utils/text.py
@@ -5,7 +5,7 @@ def translate_text(text, src, dest):
   d_code = languages.get_language_code(dest)
   try:
     res = translate.translate_single(text, s_code, d_code)
-    return {'success': True, 'result': res.text}
+    return {'success': True, 'result': res}
   except Exception as e:
     print(e)
     return {'message': f"{e}", "error": True}
--- api/utils/translate.py
+++ api/utils/translate.py
@@ -3,7 +3,7 @@ from googletrans import Translator
 translator = Translator()
 
 def translate_single(text, from_language, to_language):
-    return translator.translate(text, src=from_language, dest=to_language)
+    return translator.translate(text, src=from_language, dest=to_language).text
 
 def translate_batch(batch, to_language):
     return translator.translate(batch, dest=to_language)
