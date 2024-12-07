b1280620f32ea529aa561bb9729aab62a0f44f6d
sushma1031 <sushmajram@gmail.com>
2024-12-07 15:07:34 +0530

    fe: improve landing page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import createAPIConfig from "./config/config.js"
-import { useRouter } from 'next/navigation';
-import io from "socket.io-client";
 import { Button } from "@mui/material";
 
-interface User{
-  name: string,
-  language: string,
-  _id: string,
-  profile_pic?: string
-}
-
-export default function UserHome() {
-  return(
+export default function Home() {
+  return (
     <>
-      <h1 className="text-center py-3 text-xl">Home</h1>
-      <main className="text-center mx-auto">
-        <div className="w-full flex flex-col items-center justify-center">
-          <h1 className="text-center text-2xl">Welcome to Bridge!</h1>
-          <p className="text-center">
-            Empowering global communication, bridging cultures, and building a
-            world without language limits!
-          </p>
-          <Link
-            href={"/register"}
-            className="text-center font-medium text-blue-600 hover:underline capitalize"
-          >
-            Get Started
-          </Link>
+      <main className="mx-auto">
+        <div className="w-full flex flex-row justify-between items-center">
+          <div className="ps-16 flex flex-col h-[85vh] justify-center gap-2">
+            <h1 className="text-3xl text-center font-bold">Welcome to Bridge!</h1>
+            <p className="text-center">
+              Empowering global communication, bridging cultures, and building a
+              world without language limits
+            </p>
+            <Button
+              variant="contained"
+              className="self-center contained-button mt-8"
+            >
+              <Link
+                href={"/register"}
+                className="text-center font-medium"
+              >
+                <p className="capitalize">Get Started</p>
+              </Link>
+            </Button>
+          </div>
+          <div className="">
+            <img src="/hero.png" alt="" />
+          </div>
         </div>
       </main>
     </>
