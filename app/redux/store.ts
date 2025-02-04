f11f84f416e0fa95dc2d6e959ed8ff513ad2a3e9
sushma1031 <sushmajram@gmail.com>
2025-02-04 21:03:19 +0530

    remove unused Redux middleware

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -5,12 +5,6 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['user/setSocketConnection'],
-      },
-    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
